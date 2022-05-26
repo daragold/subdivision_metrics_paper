ee8decdb923c6ca39314ca08ae8caf2c92504b02
Amy Becker <amariah.becker@gmail.com>
2022-05-26 18:02:14 -0400

    figs path

--- MN_map_figures.py
+++ MN_map_figures.py
@@ -51,18 +51,19 @@ def draw_graph_w_division(district_df, assignment_col, color_col, fig_name, colo
     plt.close()    
 
 
+geoid = 'CNTY_VTD'
 mn_shp = gpd.read_file('./input_data/mn20_shapefile/')
 least_change_mn_plans = pd.read_csv('./input_data/least_change_plans_new.csv')
-subdiv_splits_mn_plans = pd.read_csv('./input_data/subdivision_splits_plans.csv')
+subdiv_splits_mn_plans = pd.read_csv('./input_data/mn20_out_plans_test.csv')
 mn_plan_merge = mn_shp.merge(least_change_mn_plans.rename(columns = {plan:'LC_'+plan for plan in [col for col in least_change_mn_plans.columns if 'PLAN' in col.upper()]}), how = 'left', left_on = 'VTDID', right_on = 'VTDID')
-mn_plan_merge = mn_plan_merge.merge(subdiv_splits_mn_plans.rename(columns = {plan:'SPLITS_'+plan for plan in [col for col in subdiv_splits_mn_plans.columns if 'PLAN' in col.upper()]}), how = 'left', left_on = 'VTDID', right_on = 'VTDID')
+mn_plan_merge = mn_plan_merge.merge(subdiv_splits_mn_plans.rename(columns = {plan:'SPLITS_'+plan for plan in [col for col in subdiv_splits_mn_plans.columns if col!=geoid]}), how = 'left', left_on = 'VTDID', right_on = 'VTDID')
 county_shp = mn_shp.dissolve(by = 'COUNTYFIPS').reset_index()
 
 
 figsdir = './figs/'
 os.makedirs(os.path.dirname(figsdir), exist_ok=True)
 
-for plan in ['Enacted']+[col for col in mn_plan_merge.columns if 'SPLITS_PLAN' in col.upper()]:
+for plan in ['Enacted']+[col for col in mn_plan_merge.columns if 'SPLITS_' in col.upper()]:
     draw_graph_w_division(mn_plan_merge, plan, plan, figsdir+'MN_'+plan+'_map_full.png', color_type = 'list',cmap = 'tab20', district_labels = False, division_df = county_shp, div_lw = 2, div_color = 'black',inset = None, dpi = 500)
     draw_graph_w_division(mn_plan_merge, plan, plan, figsdir+'MN_'+plan+'_map_inset.png', color_type = 'list',cmap = 'tab20', district_labels = False, division_df = county_shp, div_lw = 6, div_color = 'black', inset = ((428316,530062),(4940966,5033176)), dpi = 500)
 
@@ -70,7 +71,4 @@ for plan in ['CONGDIST']+[col for col in mn_plan_merge.columns if 'LC_PLAN' in c
     draw_graph_w_division(mn_plan_merge, plan, plan, figsdir+'MN_'+plan+'_map_full.png', color_type = 'list',cmap = 'tab20', district_labels = False, division_df = county_shp, inset = None, dpi = 500,div_lw = 0.5, div_color = 'black',dist_outline = True, dist_lw = 2, dist_color = 'black')
     draw_graph_w_division(mn_plan_merge, plan, plan, figsdir+'MN_'+plan+'_map_inset.png', color_type = 'list',cmap = 'tab20', district_labels = False, division_df = county_shp, inset = ((428316,530062),(4940966,5033176)), dpi = 500,div_lw = 1, div_color = 'black',dist_outline = True, dist_lw = 4, dist_color = 'black')
 
-
-for plan in ['SPLITS_Plan4']:
-    draw_graph_w_division(mn_plan_merge, plan, plan, figsdir+'test_MN_'+plan+'_map_full.png', color_type = 'list',cmap = 'tab20', district_labels = False, division_df = county_shp, div_lw = 0.5, div_color = 'black',inset = None, dpi = 500)
     
